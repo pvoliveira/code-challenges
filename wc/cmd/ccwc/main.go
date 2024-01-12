e654eb687af24bc7187f346b9a0e560da0e3d023
paulo oliveira <paulovt7@gmail.com>
2024-01-12 11:50:09 +0000

    feat: add option -c to count file bytes
    also you can use the DEBUG env var to print the parameters

--- wc/cmd/ccwc/main.go
+++ wc/cmd/ccwc/main.go
@@ -2,35 +2,47 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
 	"strings"
 )
 
 func main() {
-	if len(os.Args) <= 1 {
+	if len(os.Args) < 3 || os.Args[1] != "-c" {
 		fmt.Println(strings.Join(os.Args, " "))
-		fmt.Println("The correct use is `wc <filename>`.")
+		fmt.Println("The correct use is `ccwc -c <filename>`.")
 		os.Exit(1)
 	}
 
-	filename := os.Args[1]
+	if os.Getenv("DEBUG") == "true" {
+		fmt.Println("------------------------")
+		fmt.Println(os.Args[0])
+		fmt.Println(os.Args[1])
+		fmt.Println(os.Args[2])
+		fmt.Println("------------------------")
+	}
 
-	f, err := os.Open(filename)
+	filename := os.Args[2]
+
+	f, err := os.ReadFile(filename)
 	if err != nil {
-		panic(err)
+		fmt.Errorf("\nError: %v\n", err)
+		os.Exit(2)
 	}
-	defer f.Close()
+	size := len(f)
 
-	buf := make([]byte, 512)
-	size := 0
-	for {
-		s, err := f.Read(buf)
-		size += s
-		if err == io.EOF {
-			break
+	/*
+		// I guess we can use less memory
+		// if we use a buffer and sum the number of bytes read
+		buf := make([]byte, 512)
+		size := 0
+		for {
+			s, err := f.Read(buf)
+			size += s
+			if err == io.EOF {
+				break
+			}
 		}
-	}
+	*/
 
-	fmt.Printf("%v %s\n", size, filename)
+	fmt.Printf("%v\t%s\n", size, filename)
 }
